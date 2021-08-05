21ed85068ad42076eaee326a3d396510c76ee9cf
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-05 20:35:48 -0300

    Create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <form action="">
+      <input type="text" placeholder="Título" />
+      <textarea placeholder="Escreva sua nota" />
+      <button>Criar nota</button>
+    </form>
+  );
+}
+
+export default App;
