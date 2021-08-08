c251b859a86acbfb8bf273f7f12fd89f46c12201
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-08 19:56:41 -0300

    Render props in cards

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import ListaDeNotas from './components/ListaDeNotas'
 import "./assets/App.css";
 import './assets/index.css';
 
-export default function App() {
+export default class App extends Component{
 
-  criarNota(titulo, texto){
-    console.log(4);
+  constructor() {
+    super();
+    this.state = {
+      notas:[]
+    };
   }
 
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+
+    this.setState(novoEstado)
+  }
+
+  render(){
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota}/>
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
+    }
 }
 
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -6,10 +6,10 @@ export default class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("trabalho", "trabalho", "estudo").map((categorias, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} texto={nota.texto} />
             </li>
           );
         })}
