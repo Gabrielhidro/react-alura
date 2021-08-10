f76ea73304899699ae2f49d42f692100700d7fe2
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-09 22:02:13 -0300

    Import Icons

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import deleteImg from "../../assets/img/delete.svg";
+
 class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteImg} alt="" />
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
